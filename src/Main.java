cb4f6ece6e9fc92a014611a826778e78071464be
Zhelyazko Georgiev <zhelyazkogg@gmail.com>
2019-11-03 17:59:27 +0200

    improved a litte

--- src/Main.java
+++ src/Main.java
@@ -1,80 +1,44 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.net.URL;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class Main {
-    private static String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static String DB_CONNECTION = "jdbc:mysql://localhost:3306/currency";
-    private static String DB_USER = "root";
-    private static String DB_PASSWORD = "9706194543";
 
-    public static void main(String[] args) throws SQLException {
-       /* BufferedReader reader = new BufferedReader(new FileReader(
-                "E:\\MY Stuff\\Universty\\ComputerLinguistic\\currencies.txt"));*/
-
-      //   textReader(); //calling the method i guess
-
-        Scanner option = new Scanner(System.in);
-        String readOptions;
-        System.out.println("From which currency do you wish to convert to BGN?: ");
-        System.out.println("1. Euro/EUR");
-        System.out.println("2. Pound/GBP");
-        System.out.println("3. Dollar/USD");
-        System.out.println("4. Franc/CHF");
-        System.out.println("5. Cancel");
-
-        do {
-            System.out.println("Select an option:");
-            readOptions = option.next();
-
-            switch (readOptions){
-                case "1":
-                    optionEuro();
-                    break;
-                case "2":
-                    optionPound();
-                    break;
-                case "3":
-                    optionDollar();
-                    break;
-                case "4":
-                    optionFranc();
-                    break;
-                case "5":
-                    System.out.println("Canceled");
-                    break;
-                default:
-                    System.out.println("Invalid selection");
-                    break;
-            }
+    public static void main(String[] args) throws IOException {
+       try{
+           Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/currency",
+                   "root","9706194543");
+           Statement statement = conn.createStatement();
+           String sql = "SELECT * FROM currency.currencies";
+           ResultSet resultSet = statement.executeQuery(sql);
+           /* while(resultSet.next()){
+               System.out.println(resultSet.getString("EUR"));
+           } */
+       } catch (SQLException e){
+           e.printStackTrace();
         }
-        while (!readOptions.equals("5"));
-    }
 
-    private static void textReader(BufferedReader reader) throws FileNotFoundException {
-        reader = new BufferedReader(new FileReader(
-                "E:\\MY Stuff\\Universty\\ComputerLinguistic\\currencies.txt"));
-    }
+        Path path = Paths.get("E:\\My Stuff\\Universty\\ComputerLinguistic\\currencies.txt");
+        StringBuilder str = new StringBuilder();
+        try {
+         BufferedReader reader = Files.newBufferedReader(path);
+         reader.lines().forEach(s -> str.append(s + " "));
+       } catch (IOException e){
+           System.out.println(e.getMessage());
+       }
 
-    private static void optionEuro() {
-
-    }
-
-    private static void optionPound(){
-
-    }
-
-    private static void optionDollar(){
-
-    }
-
-    private static void optionFranc(){
+        Pattern pattern = Pattern.compile("([A-Z]{3})\\s\\d.+");
+        Matcher matcher = pattern.matcher(str);
+        // System.out.println(str);
+        while(matcher.find()) {
+            System.out.println(matcher.group(1));
+        }
 
     }
-
 }
