c474de0596d83107a06fae62184c69838c63cf02
Zhelyazko Georgiev <zhelyazkogg@gmail.com>
2019-11-07 20:22:44 +0200

    done.

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,15 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.*;
-import java.util.Currency;
+import java.text.DecimalFormat;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Stream;
 
 public class Main {
 
-    public class Value{
-        String currency = "bgn";
-        int value = 10;
-    }
-
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-
+        Scanner choice = new Scanner(System.in);
         try {
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/currency",
                     "root", "9706194543");
@@ -36,20 +30,45 @@ public class Main {
                 System.out.println(e.getMessage());
             }
             String regex = "(\\p{Sc}\\s\\d+[.,]?\\d+)|([A-Z]{3,}\\s\\d+[.,]?\\d+)|(\\d+[,.]?\\d+\\s[a-z]+)";
+            String text = str.toString();
             Pattern pattern = Pattern.compile(regex);
-            Matcher matcher = pattern.matcher(str);
-
+            Matcher matcher = pattern.matcher(text);
+            String currency;
+            String currencyValue;
+            String calculatedCurrency;
+            System.out.println(text);
+            DecimalFormat df = new DecimalFormat("#.##");
             while (matcher.find()) {
-                System.out.println(matcher.group());
+                while(resultSet.next()){
+                    currency = matcher.group().split("\\s")[0];
+                    currencyValue = matcher.group().split("\\s")[1];
+
+                    if (currency.equalsIgnoreCase(resultSet.getString("currency"))){
+                        calculatedCurrency =
+                                String.valueOf(df.format(Double.parseDouble(currencyValue)
+                                        * Double.parseDouble(resultSet.getString("value"))));
+
+                        text = text.replaceAll(currencyValue, calculatedCurrency);
+
+                        if (currency.matches("[A-Z]{3,}")){
+                            text = text.replaceAll(currency.trim(), "|BGN");
+                        } else {
+                            text = text.replaceAll("\\p{Sc}", "|lv");
+                        }
+                    }
+                    if (currencyValue.equalsIgnoreCase(resultSet.getString("currency"))){
+                        calculatedCurrency = String.valueOf(df.format(Double.parseDouble(currency)
+                                * Double.parseDouble(resultSet.getString("value"))));
+                        text = text.replaceAll(currency, calculatedCurrency);
+                        text = text.replaceAll(currencyValue.trim(), "leva|");
+                    }
+                }
+                resultSet.beforeFirst();
             }
+            conn.close();
+            System.out.println(text);
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-       /* Scanner choice = new Scanner(System.in);
-        System.out.println("\nWhich currency do you wish to convert into BGN?:");
-        System.out.println("1. Euro/EUR\n2. Dollars/USD\n3. Pounds/GBP\n4. Franc/CHF");
-        choice.hasNext();*/
-
     }
 }
