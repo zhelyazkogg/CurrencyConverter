57363b60e3812f09905faaf3c4d59c526724990b
Zhelyazko Georgiev <zhelyazkogg@gmail.com>
2019-11-08 18:35:13 +0200

    added comments

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,24 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.*;
 import java.text.DecimalFormat;
-import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        Scanner choice = new Scanner(System.in);
+    public static void main(String[] args) {
+        // connecting MySQL to the program
         try {
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/currency",
                     "root", "9706194543");
             Statement statement = conn.createStatement();
             String sql = "SELECT * FROM currency.currencies";
             ResultSet resultSet = statement.executeQuery(sql);
-
+        // giving the directory of the txt file
             Path path = Paths.get("E:\\My Stuff\\Universty\\ComputerLinguistic\\currencies.txt");
+        // concatinating a set of Strings
             StringBuilder str = new StringBuilder();
+        // printing out the information that was in the txt file
             try {
                 BufferedReader reader = Files.newBufferedReader(path);
                 StringBuilder finalStr = str;
@@ -29,6 +30,7 @@ public class Main {
             } catch (IOException e) {
                 System.out.println(e.getMessage());
             }
+            // using a regular expression to get only the currencies with their values
             String regex = "(\\p{Sc}\\s\\d+[.,]?\\d+)|([A-Z]{3,}\\s\\d+[.,]?\\d+)|(\\d+[,.]?\\d+\\s[a-z]+)";
             String text = str.toString();
             Pattern pattern = Pattern.compile(regex);
@@ -37,10 +39,13 @@ public class Main {
             String currencyValue;
             String calculatedCurrency;
             System.out.println(text);
+            // formating the currencies to the second number after the decimal point
             DecimalFormat df = new DecimalFormat("#.##");
             while (matcher.find()) {
                 while(resultSet.next()){
+                    // getting the first element
                     currency = matcher.group().split("\\s")[0];
+                    // getting the second element
                     currencyValue = matcher.group().split("\\s")[1];
 
                     if (currency.equalsIgnoreCase(resultSet.getString("currency"))){
